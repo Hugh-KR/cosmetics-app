76eb18dafabe8f8ebb427942287082587dd8bfaf
Hugh <lee2317884@gmail.com>
2024-10-02 09:10:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Cosmetics Application Layout
+    </div>
+  );
+}
+
+export default App;
